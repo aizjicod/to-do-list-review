cb458a5310cce0e147451e33272d06d148adea15
aizjicod <98921366+aizjicod@users.noreply.github.com>
2022-07-11 16:14:21 -0400

    add repository

---
+++ src/modules/storage.js
@@ -0,0 +1,14 @@
+// --------------------------functions--------------------------------//
+
+const setStorage = (data) => {
+  localStorage.setItem('task', JSON.stringify(data));
+};
+
+const getStorage = (data) => {
+  data.push(...JSON.parse(localStorage.getItem('task')));
+};
+
+export default {
+  setStorage,
+  getStorage,
+};
